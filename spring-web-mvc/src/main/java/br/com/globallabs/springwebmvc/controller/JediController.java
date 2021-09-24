f12136bed25e9c908345d4ca658643147754db95
Gleides Vinente da Silva <gleidevelop@gmail.com>
2021-09-24 00:36:52 -0400

    resolvendo dependencias gradle

--- spring-web-mvc/src/main/java/br/com/globallabs/springwebmvc/controller/JediController.java
+++ spring-web-mvc/src/main/java/br/com/globallabs/springwebmvc/controller/JediController.java
@@ -1,32 +1,80 @@
 package br.com.globallabs.springwebmvc.controller;
 
 import br.com.globallabs.springwebmvc.model.Jedi;
+import br.com.globallabs.springwebmvc.repository.JediRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
+import javax.validation.Valid;
+import java.util.Optional;
+
 
 @Controller
 public class JediController {
+
+    @Autowired
+    private JediRepository repository;
+
     @GetMapping("/jedi")
-    public ModelAndView jedi(){
+    public ModelAndView jedi() {
         final ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("jedi");
+        modelAndView.addObject("allJedi", repository.findAll());
 
-        final Jedi luke = new Jedi("Luke", "Skywalker");
-        modelAndView.addObject("allJedi", List.of(luke));
+        return modelAndView;
+    }
 
+    @GetMapping("/search")
+    public ModelAndView jediSearch(@RequestParam(value = "name") final String name) {
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("jedi");
+        modelAndView.addObject("allJedi", repository.findByNameContainingIgnoreCase(name));
         return modelAndView;
     }
 
     @GetMapping("/new-jedi")
-    public ModelAndView newJedi(){
-        final ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("new-jedi");
-
-        modelAndView.addObject("jedi", new Jedi());
-
-        return modelAndView;
+    public String newJedi(Model model) {
+        model.addAttribute("jedi", new Jedi());
+        return "new-jedi";
     }
+
+    @PostMapping("/jedi")
+    public String createJedi(@Valid @ModelAttribute Jedi jedi, BindingResult result, RedirectAttributes redirect) {
+        if (result.hasErrors()) {
+            if (jedi.getId() == null) {
+                return "new-jedi";
+            } else {
+                return "edit-jedi";
+            }
+        }
+        repository.save(jedi);
+        redirect.addFlashAttribute("message", "Jedi success registration");
+
+        return "redirect:jedi";
+    }
+
+    @GetMapping("/jedi/{id}/delete")
+    public String deleteJedi(@PathVariable("id") final Long id, RedirectAttributes redirect) {
+        final Optional<Jedi> jedi = repository.findById(id);
+        repository.delete(jedi.get());
+        redirect.addFlashAttribute("message", "Jedi success deleted");
+
+        return "redirect:/jedi";
+    }
+
+    @GetMapping("/jedi/{id}/update")
+    public String updateJedi(@PathVariable("id") final Long id, Model model) {
+
+        final Optional<Jedi> jedi = repository.findById(id);
+
+        model.addAttribute("jedi", jedi.get());
+
+        return "edit-jedi";
+    }
+
 }
---
+++ spring-web-mvc/src/main/java/br/com/globallabs/springwebmvc/rest/JediResource.java
@@ -0,0 +1,36 @@
+package br.com.globallabs.springwebmvc.rest;
+
+import br.com.globallabs.springwebmvc.model.Jedi;
+import br.com.globallabs.springwebmvc.repository.JediRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class JediResource {
+
+    @Autowired
+    private JediRepository repository;
+
+    @GetMapping("/api/jedi")
+    public List<Jedi> getAllJedi(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/api/jedi/{id}")
+    public ResponseEntity<Jedi> getJedi(@PathVariable("id") Long id){
+        final Optional<Jedi> jedi = repository.findById(id);
+
+        if (jedi.isPresent()) {
+            return ResponseEntity.ok(jedi.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
